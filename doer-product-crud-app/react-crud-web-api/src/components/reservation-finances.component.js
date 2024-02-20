1802de68f5679a4f2e6cb466d8ff6d63f3577919
fakirnooruddin <fsnooruddin@localhost.com>
2024-02-20 19:11:55 +0500

    adding images for web app

--- doer-product-crud-app/react-crud-web-api/src/components/reservation-finances.component.js
+++ doer-product-crud-app/react-crud-web-api/src/components/reservation-finances.component.js
@@ -10,11 +10,9 @@ export default class ReservationsFinances extends Component {
     super(props);
     this.onChangeSearchAvailability = this.onChangeSearchAvailability.bind(this);
     this.onChangeSearchServices = this.onChangeSearchServices.bind(this);
-    this.retrieveReservations = this.retrieveReservations.bind(this);
+
     this.refreshList = this.refreshList.bind(this);
-    this.scheduleDoers = this.scheduleDoers.bind(this);
-    this.searchAvailability = this.searchAvailability.bind(this);
-    //this.searchServices = this.searchServices.bind(this);
+
 
     this.state = {
       reservationRequests: [],
@@ -26,8 +24,9 @@ export default class ReservationsFinances extends Component {
   }
 
   componentDidMount() {
-    this.retrieveReservations();
+
   }
+
 
   onChangeSearchAvailability(e) {
     const searchAvailability = e.target.value;
@@ -44,47 +43,15 @@ export default class ReservationsFinances extends Component {
     }
 
   refreshList() {
-    this.retrieveReservations();
+
     this.setState({
       currentDoers: [],
       currentReservations: []
     });
   }
 
-scheduleDoers() {
 
-   console.log(this.state.currentDoers);
-   // alert("Sent Scheduling Requests!");
-    // Get the modal
-    var modal = document.getElementById("overlay-content");
-    modal.style.display = "block";
 
-    DoerDataService.scheduleDoers(this.state.currentDoers, this.state.searchAvailability);
-
-    var doers_list = document.getElementsByClassName("doersRow");
-    for(let i=0;i<doers_list.length;i++) {
-        doers_list[i].bgColor = "";
-        doers_list[i].childNodes[0].className = "cell-name-highlight";
-    }
-  }
-
-  retrieveReservations() {
-    console.log("In reservation-find.componennt.js retreiveReservations");
-    console.log("this.state");
-    console.log(this.state);
-    DoerDataService.getAllReservationsRequests()
-         .then(response => {
-            console.log("response from DoerDataService.getAllReservationsRequests is");
-            console.log(response);
-            this.setState({
-              reservationRequests: response.data
-            });
-            console.log(response.data);
-          })
-          .catch(e => {
-            console.log(e);
-          });
-  }
 
  closeDialog() {
     // Get the modal
@@ -92,83 +59,6 @@ scheduleDoers() {
     modal.style.display = "none";
   }
 
-  getDayFromAvailability(availability) {
-    const retArray = availability.split(":");
-        return retArray[0];
-  }
-
-  getTimeFromAvailability(availability) {
-    const retArray = availability.split(":");
-    if(retArray[1] === undefined) {
-        return null;
-    } else {
-        return retArray[1];
-    }
-  }
-
-  timesMatch(reqSlotTime, slotTime) {
-
-    let retArray=slotTime.split("-");
-    let doerStartTime = parseInt(retArray[0]);
-    let doerCloseTime = parseInt(retArray[1]);
-    console.log("doer start time = " + doerStartTime);
-    console.log("doer end time = " + doerCloseTime);
-
-    retArray=reqSlotTime.split("-");
-    let reqStartTime = parseInt(retArray[0]);
-    let reqCloseTime = parseInt(retArray[1]);
-    console.log("req start time = " + reqStartTime);
-    console.log("req end time = " + reqCloseTime);
-
-	if(reqStartTime >= doerStartTime) {
-      console.log("start times match...");
-      if(reqCloseTime <= doerCloseTime) {
-     	 console.log("close times match...");
-      	 return true;
-      }
-    }
-    return false;
-  }
-
-filterResultsByTime(tutorials) {
-    if(this.state.searchAvailability == null) {
-        return tutorials;
-    }
-
-    const filteredDoers = tutorials.filter(filterTimeFunction, this);
-
-    function filterTimeFunction(value, index, array) {
-        console.log("...filtering...");
-        console.log(value);
-        //console.log(time);
-        console.log(this.state);
-        return utils.processTimeMatch(this.state.searchAvailability, value.availability);
-    }
-
-    return filteredDoers;
-}
-
- searchAvailability() {
-     this.setState({
-	 currentDoers: [],
-     });
-
-     DoerDataService.findByAvailability(this.state.searchAvailability)
-	 .then(response => {
-	 	     console.log("in search availability ... DB response response");
-     	     console.log(response.data);
-
-             const filteredResponse = this.filterResultsByTime(response.data);
-             this.setState({
-		        tutorials: filteredResponse
-             });
-	     console.log("in search availability ... filtered response");
-	     console.log(filteredResponse);
-	 })
-	 .catch(e => {
-             console.log(e);
-	 });
-}
 
 getActiveLabel(event, tutorial)
 {
@@ -182,13 +72,6 @@ getActiveLabel(event, tutorial)
     }
   }
   this.state.currentDoers.push(tutorial);
-}
-
-getJSDateTime(mysqlDateTime)
-{
- var t = mysqlDateTime.split("T");
- var r = t[1].split(".");
- return (t[0]+ " " + r[0]);
 }
 
   render() {
