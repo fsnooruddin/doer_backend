bdffdbffb3fd2c5fc2a0d8481830c554fb2b706a
fsnooruddin <fsnooruddin@hotmail.com>
2024-04-10 15:49:21 -0700

    reformatted file

--- doer-product-crud-app/nodejs-express-sequelize-mysql/server.js
+++ doer-product-crud-app/nodejs-express-sequelize-mysql/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: ['http://127.0.0.1:8081', 'http://localhost:8081']
+    origin: ['http://127.0.0.1:8081', 'http://localhost:8081']
 };
 
 app.use(cors(corsOptions));
@@ -13,17 +13,19 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 const db = require("./app/models");
 
 db.sequelize.sync()
-  .then(() => {
-    console.log("Synced db.");
-  })
-  .catch((err) => {
-    console.log("Failed to sync db: " + err.message);
-  });
+    .then(() => {
+        console.log("Synced db.");
+    })
+    .catch((err) => {
+        console.log("Failed to sync db: " + err.message);
+    });
 
 // // drop the table if it already exists
 // db.sequelize.sync({ force: true }).then(() => {
@@ -32,7 +34,9 @@ db.sequelize.sync()
 
 // simple route
 app.get("/doer", (req, res) => {
-  res.json({ message: "Welcome to doer application." });
+    res.json({
+        message: "Welcome to doer application."
+    });
 });
 
 require("./app/routes/doer.routes")(app);
@@ -40,5 +44,5 @@ require("./app/routes/doer.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
