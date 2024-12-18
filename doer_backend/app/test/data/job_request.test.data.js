dfcf66dc1d8e09ee7f612dbf80a5a89652413d63
fsnooruddin <fsnooruddin@hotmail.com>
2024-12-18 10:55:06 -0800

    updating tests

---
+++ doer_backend/app/test/data/job_request.test.data.js
@@ -0,0 +1,9 @@
+const reqCreateJobRequest = {
+ "location": "94588",
+  "time": "M, 12-5",
+  "services": "Masonry/Concrete"
+}
+
+module.exports = {
+  reqCreateJobRequest
+}
---
+++ doer_backend/app/test/job_request.test.js
@@ -0,0 +1,37 @@
+
+
+const request = require('supertest')('http://localhost:3721/api/doer');
+//const app = require('../../server.js'); // Your Express app
+// var expect = require('chai').expect;
+     const { expect } = require('chai');
+
+
+const { reqCreateJobRequest } = require("./data/job_request.test.data.js");
+
+describe('create job requests', () => {
+
+  it('should create a new JobRequest', async () => {
+    const res = await request
+      .post('/createJobRequest')
+      .send(reqCreateJobRequest)
+      .set('Accept', 'application/json');
+
+     expect(res.body).to.have.property("job_request_id");
+     console.log(res.body);
+  });
+
+});
+
+describe('modify job requests', function() {
+
+      it('accept job request', async() => {
+        const res = await request.post('/acceptJob?doerId=1&jobId=1');
+        console.log(res.body);
+      });
+
+      it('complete job requests', async() => {
+        const res = await request.post('/completeJob?doerId=1&jobId=1');
+        console.log(res.body);
+      });
+
+});
