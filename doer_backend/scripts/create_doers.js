3f05e1a71848445fa96907d375b479f802fa06ea
fsnooruddin <fsnooruddin@hotmail.com>
2024-12-31 18:14:51 -0800

    fixing bugs

--- doer_backend/scripts/create_doers.js
+++ doer_backend/scripts/create_doers.js
@@ -1,51 +1,54 @@
 "use strict";
-const request = require('superagent');
-const fs = require('fs');
+//import fetch from 'node-fetch';
+const fs = require("fs");
+const axios = require("axios");
 
+async function rpost(url, dataString) {
+  const response = await axios.post(
+    "http://localhost:8080/api/doer/createDoer",
+    dataString,
+  );
+
+  return response.data;
+}
 
 // Find a single Doer by services
 async function createDoer(entry) {
-var uri = 'http://localhost:8080/api/doer/createDoer';
+  var uri = "http://localhost:8080/api/doer/createDoer";
 
-    try {
+  try {
     console.log("create Doer entry = " + JSON.stringify(entry));
-    const response_data = await request.post(uri).send(entry);
+    const response_data = await rpost(uri, entry);
 
-       console.log("response.body     " + response_data.body);
-       console.log("createDoer response = " + JSON.stringify(response_data.body) + "    " );
-       return true;
-      }
-      catch(error)  {
-        console.log("Couldn't create Doer..." + JSON.stringify(entry));
-        console.error(error);
-        return false;
-      }
-
+    console.log("response.body     " + response_data);
+    console.log(
+      "createDoer response = " + JSON.stringify(response_data) + "    ",
+    );
+    return true;
+  } catch (error) {
+    console.log("Couldn't create Doer..." + JSON.stringify(entry));
+    console.error(error);
+    return false;
+  }
 }
 
 async function main() {
+  // Accessing command line arguments in Node.js
+  const args = process.argv.slice(2);
 
-    // Accessing command line arguments in Node.js
-    const args = process.argv.slice(2);
+  console.log("args = ");
+  console.log(args);
 
-    console.log("args = ");
-    console.log(args);
+  let rawdata = fs.readFileSync(args[0]);
+  let doer_data = JSON.parse(rawdata);
 
-    let rawdata = fs.readFileSync(args[0]);
-    let doer_data = JSON.parse(rawdata);
+  console.log(JSON.stringify(doer_data));
 
-    console.log(JSON.stringify(doer_data));
-
-    for(let i=0;i<doer_data.doers.length;i++) {
-            var entry = doer_data.doers[i];
-            entry.minimum_charges = entry.min_charges;
-            createDoer(entry);
-       }
-
+  for (let i = 0; i < doer_data.doers.length; i++) {
+    var entry = doer_data.doers[i];
+    entry.minimum_charges = entry.min_charges;
+    createDoer(entry);
+  }
 }
 
-
 main();
-
-
-
