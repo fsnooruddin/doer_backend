2736a1d7a57342f3cab7c6e64b978c36e410a6a4
fsnooruddin <fsnooruddin@hotmail.com>
2025-01-06 17:00:23 -0800

    increasing size of string columns

--- doer_backend/app/models/doer.model.js
+++ doer_backend/app/models/doer.model.js
@@ -18,11 +18,11 @@ module.exports = (sequelize, Sequelize) => {
 			allowNull: false,
 		},
 		services: {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(2048),
 			allowNull: false,
 		},
 		availability: {
-			type: Sequelize.STRING,
+			type: Sequelize.STRING(2048),
 			allowNull: false,
 		},
 		rating: {
