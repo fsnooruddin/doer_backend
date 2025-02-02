332643dfde29942824a40d792b95c0584bceb3dc
fsnooruddin <fsnooruddin@hotmail.com>
2025-02-01 19:10:38 -0800

    configuring winston

--- doer_backend/app/utils/Logger.js
+++ doer_backend/app/utils/Logger.js
@@ -1,33 +1,32 @@
-const pino = require("pino");
+const winston = require("winston");
 
-const transports = pino.transport({
-	targets: [
-		{
-			level: "trace",
-			target: "pino/file",
-			options: { destination: "./logs/log.txt" },
-		},
-		{
-			level: "trace",
-			target: "pino-pretty",
-			options: {
-				colorize: true,
-			},
-		},
+const logLevels = {
+	fatal: 0,
+	error: 1,
+	warn: 2,
+	info: 3,
+	debug: 4,
+	trace: 5,
+};
+
+const { combine, timestamp, printf, colorize, align } = winston.format;
+
+const logger = winston.createLogger({
+	levels: logLevels,
+	level: process.env.LOG_LEVEL || "trace",
+	format: combine(
+		timestamp({
+			format: "YYYY-MM-DD hh:mm:ss.SSS A",
+		}),
+		align(),
+		printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+	),
+	transports: [
+		new winston.transports.Console(),
+		new winston.transports.File({
+			filename: "combined.log",
+		}),
 	],
 });
-
-const logger = pino(
-	{
-		level: "trace",
-		timestamp: pino.stdTimeFunctions.isoTime,
-		formatters: {
-			bindings: (bindings) => {
-				return {};
-			},
-		},
-	},
-	transports
-);
 
 module.exports = logger;
