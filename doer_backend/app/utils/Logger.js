b82bc74a381ed6e2415171a98c379bf356aa8932
fsnooruddin <fsnooruddin@hotmail.com>
2025-02-01 20:00:49 -0800

    adjusting winston colors

--- doer_backend/app/utils/Logger.js
+++ doer_backend/app/utils/Logger.js
@@ -9,17 +9,29 @@ const logLevels = {
 	trace: 5,
 };
 
+const color = {
+'fatal': "\x1b[38;5;196m",
+'error': "\x1b[41m",
+'warn': "\x1b[38;5;166m",
+'info': "\x1b[38;5;214m",
+'debug': "\x1b[38;5;33m",
+'trace': "\x1b[38;5;118m"
+};
+
 const { combine, timestamp, printf, colorize, align } = winston.format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+return `${level}: ${color[level] || ''} ${timestamp} || ${message}\x1b[0m `;
+});
 
 const logger = winston.createLogger({
 	levels: logLevels,
-	level: process.env.LOG_LEVEL || "trace",
+	level: process.env.LOG_LEVEL || "info",
 	format: combine(
 		timestamp({
 			format: "YYYY-MM-DD hh:mm:ss.SSS A",
 		}),
-		align(),
-		printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+		myFormat
 	),
 	transports: [
 		new winston.transports.Console(),
