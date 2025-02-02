3074adee20ba41bfb1a3129fcb770ee895a43da2
fsnooruddin <fsnooruddin@hotmail.com>
2025-02-01 17:55:09 -0800

    sending sequelize logs to pino

--- doer_backend/app/models/index.js
+++ doer_backend/app/models/index.js
@@ -1,9 +1,13 @@
 const dbConfig = require("../config/db.config.js");
+const logger = require("../utils/Logger.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
 	host: dbConfig.HOST,
 	dialect: dbConfig.dialect,
+	logging: (query, options) => {
+		logger.info(`${query} [${options.modelName}]`);
+	},
 });
 
 const db = {};
