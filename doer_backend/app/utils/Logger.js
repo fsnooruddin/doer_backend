1b0cc65c295b056242d0d0532c8996f4302483a3
fsnooruddin <fsnooruddin@hotmail.com>
2025-02-04 10:29:16 -0800

    Adjust logging colors

--- doer_backend/app/utils/Logger.js
+++ doer_backend/app/utils/Logger.js
@@ -1,44 +1,36 @@
 const winston = require("winston");
 
-const logLevels = {
-	fatal: 0,
-	error: 1,
-	warn: 2,
-	info: 3,
-	debug: 4,
-	trace: 5,
-};
+const myCustomLevels = {
+	logLevels: {
+		fatal: 0,
+		error: 1,
+		warn: 2,
+		info: 3,
+		debug: 4,
+		trace: 5,
+	},
 
-const color = {
-'fatal': "\x1b[38;5;196m",
-'error': "\x1b[41m",
-'warn': "\x1b[38;5;166m",
-'info': "\x1b[38;5;214m",
-'debug': "\x1b[38;5;33m",
-'trace': "\x1b[38;5;118m"
+	colors: {
+		fatal: "\x1b[38;5;196m",
+		error: "\x1b[41m",
+		warn: "\x1b[38;5;166m",
+		info: "\x1b[38;5;214m",
+		debug: "\x1b[38;5;33m",
+		trace: "\x1b[38;5;118m",
+	},
 };
 
 const { combine, timestamp, printf, colorize, align } = winston.format;
 
-const myFormat = printf(({ level, message, label, timestamp }) => {
-return `${level}: ${color[level] || ''} ${timestamp} || ${message}\x1b[0m `;
+const myFormat = printf(({ level, message, timestamp }) => {
+	return `${level} ${myCustomLevels.colors[level] || ""} || ${timestamp} || ${message} \x1b[0m`;
 });
 
 const logger = winston.createLogger({
-	levels: logLevels,
-	level: process.env.LOG_LEVEL || "info",
-	format: combine(
-		timestamp({
-			format: "YYYY-MM-DD hh:mm:ss.SSS A",
-		}),
-		myFormat
-	),
-	transports: [
-		new winston.transports.Console(),
-		new winston.transports.File({
-			filename: "/var/log/doer/combined.log",
-		}),
-	],
+	levels: myCustomLevels.logLevels,
+	level: process.env.LOG_LEVEL || "trace",
+	format: combine(timestamp({ format: "YYYY-MM-DD hh:mm:ss.SSS A" }), myFormat),
+	transports: [new winston.transports.Console({ eol: "\n" }), new winston.transports.File({ filename: "/var/log/doer/combined.log" })],
 });
 
 module.exports = logger;
