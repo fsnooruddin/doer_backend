d71837d9f90d23e09a6fac1339e6abaaee65fd4c
fsnooruddin <fsnooruddin@hotmail.com>
2025-02-11 11:22:06 -0800

    Adding more negative tests.

--- doer_backend/app/test/doer.spec.js
+++ doer_backend/app/test/doer.spec.js
@@ -3,20 +3,19 @@ var { expect, jest, test } = require("@jest/globals");
 
 const { reqCreateDoer_1, reqCreateDoer_2 } = require("./data/doer.test.data.js");
 var {
-        getDoerByIdRequestUri,
-        getDoerByServicesRequestUri,
-        getDoerByServicesAndDayRequestUri,
-        getReviewsForDoerRequestUri,
-        getReviewByIdRequestUri,
-        rateDoerRequestUri,
-        createDoerReviewUri,
-        createDoerTripUri,
-        updateDoerTripUri,
-        completeDoerTripUri,
-        updateDoerAvailabilityUri,
-        createDoerUri
+	getDoerByIdRequestUri,
+	getDoerByServicesRequestUri,
+	getDoerByServicesAndDayRequestUri,
+	getReviewsForDoerRequestUri,
+	getReviewByIdRequestUri,
+	rateDoerRequestUri,
+	createDoerReviewUri,
+	createDoerTripUri,
+	updateDoerTripUri,
+	completeDoerTripUri,
+	updateDoerAvailabilityUri,
+	createDoerUri,
 } = require("./data/test.uris.js");
-
 
 async function getData(url) {
 	try {
@@ -29,7 +28,7 @@ async function getData(url) {
 
 describe("DOER API Tests -- Successful calls", () => {
 	test("Create a new doer", async () => {
-	    console.log("URL = " + createDoerUri);
+		console.log("URL = " + createDoerUri);
 		const res = await request.post(createDoerUri).send(reqCreateDoer_1).set("Accept", "application/json");
 		//   console.log(res.body);
 		expect(res.status).toBe(200);
@@ -38,7 +37,7 @@ describe("DOER API Tests -- Successful calls", () => {
 
 	test("Search Doers by Service", async () => {
 		console.log("URL = " + getDoerByServicesRequestUri + "?services=electric");
-	//	const res = await getData(getDoerByServicesRequestUrl + "?services=electric");
+		//	const res = await getData(getDoerByServicesRequestUrl + "?services=electric");
 		const res = await getData("/getDoerByServices" + "?services=electric");
 		console.log(res.body);
 		expect(res.status).toBe(200);
@@ -46,14 +45,14 @@ describe("DOER API Tests -- Successful calls", () => {
 	});
 
 	test("Search Doers by Day and Service", async () => {
-		const res = await request.get(getDoerByServicesAndDayRequestUri +  "?day=fri&services=electric");
+		const res = await request.get(getDoerByServicesAndDayRequestUri + "?day=fri&services=electric");
 		//    console.log(res.body);
 		expect(res.status).toBe(200);
 		expect(JSON.stringify(res.body)).toContain("doer_id");
 	});
 
 	test("Get Doer by ID", async () => {
-		const res = await request.get(getDoerByIdRequestUri + "?id=2");
+		const res = await request.get(getDoerByIdRequestUri + "?id=1");
 		//  console.log(res.body);
 		expect(res.status).toBe(200);
 		expect(JSON.stringify(res.body)).toContain("doer_id");
@@ -68,15 +67,29 @@ describe("DOER API Tests -- UnSuccessful calls", function () {
 
 	test("Search Doers by Service, missing service", async () => {
 		const res = await request.get(getDoerByServicesRequestUri);
-		  //console.log(res.body);
+		//console.log(res.body);
 		expect(res.status).toBe(500);
 		expect(JSON.stringify(res.body)).toContain("message");
 	});
 
 	test("Search Doers by Service, service not string", async () => {
-    		const res = await request.get(getDoerByServicesRequestUri + "?service=827727");
-    		  //console.log(res.body);
-    		expect(res.status).toBe(500);
-    		expect(JSON.stringify(res.body)).toContain("message");
-    	});
+		const res = await request.get(getDoerByServicesRequestUri + "?service=827727");
+		//console.log(res.body);
+		expect(res.status).toBe(500);
+		expect(JSON.stringify(res.body)).toContain("message");
+	});
+
+	test("Get Doer by ID, missing Doer ID", async () => {
+		const res = await request.get(getDoerByIdRequestUri);
+		//  console.log(res.body);
+		expect(res.status).toBe(500);
+		expect(JSON.stringify(res.body)).toContain("message");
+	});
+
+	test("Get Doer by ID, Doer ID not STRING", async () => {
+		const res = await request.get(getDoerByIdRequestUri + "?id=shshhs");
+		//  console.log(res.body);
+		expect(res.status).toBe(500);
+		expect(JSON.stringify(res.body)).toContain("message");
+	});
 });
