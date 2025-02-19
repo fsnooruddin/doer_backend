c59031e0aedad1b765156be375a360314b9409bb
fsnooruddin <fsnooruddin@hotmail.com>
2025-02-18 20:35:53 -0800

    fixing bugs with create and upate

--- doer_backend/app/controllers/doer_trip.controller.js
+++ doer_backend/app/controllers/doer_trip.controller.js
@@ -4,7 +4,6 @@
  * @namespace DoerTrip
  */
 
-
 const db = require("../models");
 const Utils = require("../utils/Utils.js");
 const Doer = db.doers;
@@ -36,35 +35,23 @@ const logger = require("../utils/Logger.js");
    }
  * @memberof DoerTrip
  */
-function startDoerTrip(req, res) {
-	console.log("req body in create Doer Trip: ");
-	console.log(req.body);
+async function startDoerTrip(req, res) {
+	logger.info("Doer Trip-controller create, body is: " + req.body);
 
 	const data_obj = JSON.parse(Utils.escapeJSONString(JSON.stringify(req.body)));
 
-	/*
-    if(validation.error === undefined) {
-        console.log("doer schema validation succeeded");
-    } else {
-        console.log("\t doer schema validation failed");
-        console.log(validation.error.details[0].message);
-        res.status(400).send({
-                    message: "input data failed doer scheme validation: " + validation.error.details[0].message
-                });
-        return;
-    }
-*/
 
-	// Save DoerTrip in the database
-	DoerTrip.create(data_obj)
-		.then((data) => {
-			res.send(data);
-		})
-		.catch((err) => {
-			res.status(500).send({
-				message: err.message || "Some error occurred while creating the doer trip.",
-			});
-		});
+	try {
+		// Save DoerTrip in the database
+		const response_data = await DoerTrip.create(data_obj);
+		logger.info("Doer Trip-controller create data from create is " + response_data);
+		res.status(200).send(response_data);
+		return;
+	} catch (err) {
+		logger.error("Doer Trip-controller create, error from create is " + err.message);
+		res.status(500).send({ message: err.message || "Some error occurred while creating the doer trip." });
+		return;
+	}
 }
 
 /**
@@ -73,25 +60,32 @@ function startDoerTrip(req, res) {
  * @return {string|null} Review - null if failure, JSON object representing doer trips if success
  * @memberof DoerTrip
  */
-function findByDoerId(req, res) {
+async function findByDoerId(req, res) {
 	const id = req.query.id;
-	console.log("Doer-controller findOne id = " + id);
-	Doer.findOne({
-		where: {
-			doer_id: id,
-		},
-		attributes: {
-			exclude: ["updatedAt", "createdAt"],
-		},
-	})
-		.then((data) => {
-			res.send(data);
-		})
-		.catch((err) => {
-			res.status(500).send({
-				message: "Error retrieving Doer with id=" + id + " error: " + err.message,
-			});
+	logger.info("DoerTrip-controller findOne id = " + id);
+	try {
+		const data = await DoerTrip.findOne({
+			where: {
+				doer_id: id,
+			},
+			attributes: {
+				exclude: ["updatedAt", "createdAt"],
+			},
 		});
+
+		if (data == null) {
+			logger.error("DoerTrip-controller couldn't find trip with doerId " + id);
+			return null;
+		}
+		logger.info("doer-trip-controller findByDoerId -- doer id is " + id + " returning " + JSON.stringify(data));
+		res.status(200).send(data);
+		return;
+	} catch (err) {
+		logger.error("Doer Trip-controller create, error from create is " + err.message);
+		res.status(500).send({
+			message: "Error retrieving Doer with id=" + id + " error: " + err.message,
+		});
+	}
 }
 
 /**
@@ -103,23 +97,23 @@ function findByDoerId(req, res) {
 async function updateDoerTripLocation(req, res) {
 	const id = req.body.id;
 
-	console.log("Doer Trip-controller updateDoerTripLocation id = " + id);
+	logger.info("Doer Trip-controller updateDoerTripLocation id = " + id);
 
 	const update = {
 		doer_trip_id: id,
 		location_update: req.body.location_update,
 	};
 
-	const doer = await DoerTripLocationUpdate.create(update)
-		.then((data) => {
-			console.log("Doer Trip-controller updated location for DoerTrip id = " + id);
-			res.send(data);
-		})
-		.catch((err) => {
-			res.status(500).send({
-				message: "Error updating location for trip with id =" + id + " error: " + err.message,
-			});
-		});
+	try {
+		const data = await DoerTripLocationUpdate.create(update);
+		logger.info("Doer Trip-controller updated location for DoerTrip id = " + id);
+		res.status(200).send(data);
+		return;
+	} catch (err) {
+	    logger.error("Doer Trip-controller updated location for DoerTrip id = " + id + " Failed ... error is: " + err.message);
+		res.status(500).send({ message: "Error updating location for trip with id =" + id + " error: " + err.message });
+		return;
+	}
 }
 
 /**
@@ -128,49 +122,49 @@ async function updateDoerTripLocation(req, res) {
  * @return {string|null} Review - null if failure, JSON object representing doer trips if success
  * @memberof DoerTrip
  */
-function getDoerTripByJobId(req, res) {
+async function getDoerTripByJobId(req, res) {
 	const id = req.query.jobId;
 	if (id == null) {
 		logger.error("Error retrieving doer trips by ID, job Id is missing");
-		res.status(500).send({
+		res.status(400).send({
 			message: "Error retrieving doer trips by id, job Id is missing",
 		});
 
 		return;
 	}
 	logger.info("doer-trip-controller findById job id = " + id);
-	console.log(typeof id);
-	console.log(parseInt(id));
+	logger.info(typeof id);
+	logger.info(parseInt(id));
 
 	if (isNaN(parseInt(id))) {
 		logger.error("Error retrieving doer trips by ID, review Id is not integer, id = " + id);
-		res.status(500).send({
+		res.status(400).send({
 			message: "Error retrieving doer trips by id, review Id is not integer",
 		});
 
 		return;
 	}
 
-	DoerTrip.findAll({
-		where: {
-			job_id: id,
-		},
-		attributes: {
-			exclude: ["updatedAt", "createdAt"],
-		},
-	})
-		.then((data) => {
-			logger.info("doer-trip-controller findByJobId -- job id is " + id + " returning " + data);
-			res.status(200).send(data);
-			return;
-		})
-		.catch((err) => {
-			logger.error("Error retrieving doer trips with job id=" + id + " error: " + err.message);
-			res.status(500).send({
-				message: "Error retrieving doer trips with job id=" + id + " error: " + err.message,
-			});
-			return;
+	try {
+		const data = await DoerTrip.findAll({
+			where: {
+				job_id: id,
+			},
+			attributes: {
+				exclude: ["updatedAt", "createdAt"],
+			},
 		});
+
+		logger.info("doer-trip-controller findByJobId -- job id is " + id + " returning " + JSON.stringify(data));
+		res.status(200).send(data);
+		return;
+	} catch (err) {
+		logger.error("Error retrieving doer trips with job id=" + id + " error: " + err.message);
+		res.status(500).send({
+			message: "Error retrieving doer trips with job id=" + id + " error: " + err.message,
+		});
+		return;
+	}
 }
 
 /**
@@ -178,47 +172,47 @@ function getDoerTripByJobId(req, res) {
  * @param {number} id - Id of trip
  * @memberof DoerTrip
  */
-function completeDoerTrip(req, res) {
+async function completeDoerTrip(req, res) {
 	const id = req.query.id;
 	if (id == null) {
 		logger.error("Complete Doer trip error, doer trip Id is missing");
-		res.status(500).send({
+		res.status(400).send({
 			message: "Complete Doer trip error, doer trip Id is missing",
 		});
 
 		return;
 	}
 	logger.info("doer-trip-controller completeDoerTrip doer trip id = " + id);
-	console.log(typeof id);
-	console.log(parseInt(id));
+	logger.info(typeof id);
+	logger.info(parseInt(id));
 
 	if (isNaN(parseInt(id))) {
 		logger.error("Error completing doer trips by ID, trip Id is not integer, id = " + id);
-		res.status(500).send({
+		res.status(400).send({
 			message: "Error completing doer trips by id, trip Id is not integer",
 		});
 
 		return;
 	}
 
-	DoerTrip.update( {status: "completed"}, { where: {doer_trip_id: id} } )
-		.then((data) => {
-			logger.info("doer-trip-controller completeDoerTrip -- DoerTrip id is " + id + " returning " + "success");
-			res.status(200).send("success");
-			return;
-		})
-		.catch((err) => {
-			logger.error("Error completing doer trips with DoerTrip id=" + id + " error: " + err.message);
-			res.status(500).send({
-				message: "Error completing doer trips with DoerTrip id=" + id + " error: " + err.message,
-			});
-			return;
+	try {
+		const data = await DoerTrip.update({ status: "completed" }, { where: { doer_trip_id: id } });
+
+		logger.info("doer-trip-controller completeDoerTrip -- DoerTrip id is " + id + " returning " + "success");
+		res.status(200).send("success");
+		return;
+	} catch (err) {
+		logger.error("Error completing doer trips with DoerTrip id=" + id + " error: " + err.message);
+		res.status(500).send({
+			message: "Error completing doer trips with DoerTrip id=" + id + " error: " + err.message,
 		});
+		return;
+	}
 }
 
 module.exports = {
 	startDoerTrip,
 	updateDoerTripLocation,
 	getDoerTripByJobId,
-	completeDoerTrip
+	completeDoerTrip,
 };
