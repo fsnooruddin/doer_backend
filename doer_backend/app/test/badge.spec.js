1da8b74c47d79407b314d93638e4bd941478dfc4
fsnooruddin <fsnooruddin@hotmail.com>
2025-02-24 12:33:50 -0800

    adding badge tests

--- doer_backend/app/test/badge.spec.js
+++ doer_backend/app/test/badge.spec.js
@@ -1,7 +1,15 @@
 const request = require("supertest")("http://127.0.0.1:8080/api/doer");
 var { expect, jest, test } = require("@jest/globals");
 
-const { reqCreateBadge_1, reqCreateBadge_Malformed } = require("./data/badge.test.data.js");
+var {
+	reqCreateBadge_1,
+	reqCreateBadge_2,
+	reqCreateBadgeAssociation_1,
+	reqCreateBadgeAssociation_2,
+	reqCreateBadge_Malformed,
+	reqCreateBadgeAssociation_Malformed,
+} = require("./data/badge.test.data.js");
+
 var {
 	getCategoryByIdRequestUri,
 	getCategoryByNameRequestUri,
@@ -35,6 +43,7 @@ var {
 	getUserByIdUri,
 	createBadgeUri,
 	getBadgeByIdUri,
+	assignBadgeUri
 } = require("./data/test.uris.js");
 
 async function getData(url) {
@@ -47,47 +56,57 @@ async function getData(url) {
 }
 
 describe("BADGE API Tests -- Successful calls", () => {
-	test("Create a new badge", async () => {
+	test("Create a new BADGE", async () => {
 		const res = await request.post(createBadgeUri).send(reqCreateBadge_1).set("Accept", "application/json");
 		expect(res.status).toBe(200);
 		expect(JSON.stringify(res.body)).toContain("badge_id");
 	});
 
-	test("Get Badge by ID", async () => {
-		const res = await request.get(getBadgeByIdUri + "?id=1");
-		//  console.log(res.body);
+	test("Create a new BADGE", async () => {
+		const res = await request.post(createBadgeUri).send(reqCreateBadge_2).set("Accept", "application/json");
 		expect(res.status).toBe(200);
 		expect(JSON.stringify(res.body)).toContain("badge_id");
+	});
+
+	test("Get BADGE by ID", async () => {
+		const res = await request.get(getBadgeByIdUri + "?id=1");
+		console.log(res.body);
+		expect(res.status).toBe(200);
+		expect(JSON.stringify(res.body)).toContain("badge_id");
+	});
+
+	test("Create a BADGE association", async () => {
+
+		const res = await request.post(assignBadgeUri).send(reqCreateBadgeAssociation_1).set("Accept", "application/json");
+console.log(res.body);
+		expect(res.status).toBe(200);
+expect(JSON.stringify(res.body)).toContain("id");
 	});
 });
 
 describe("BADGE API Tests -- UnSuccessful calls", function () {
-	/*
+
+	test("Create a new BADGE, malformed body", async () => {
+    		const res = await request.post(createBadgeUri).send(reqCreateBadge_Malformed).set("Accept", "application/json");
+    		expect(res.status).toBe(500);
+    	});
+
+    		test("Create a new BADGE association, malformed body", async () => {
+            		const res = await request.post(createBadgeUri).send(reqCreateBadgeAssociation_Malformed).set("Accept", "application/json");
+            		expect(res.status).toBe(500);
+            	});
+
 	test("Get Badge by ID, missing ID", async () => {
 		const res = await request.get(getBadgeByIdUri);
-
-		expect(res.status).toBe(500);
+		expect(res.status).toBe(400);
 	});
 
 	test("Get Badge by ID,Badge ID not INTEGER", async () => {
 		const res = await request.get(getBadgeByIdUri + "?id=shshhs");
 		//  console.log(res.body);
 		expect(res.status).toBe(500);
-		expect(JSON.stringify(res.body)).toContain("badge");
+
 	});
 
-	test("Get Badge by JOB ID, missing JOB ID", async () => {
-		const res = await request.get(getBadgeByJobIdUri);
-		//  console.log(res.body);
-		expect(res.status).toBe(500);
-		expect(JSON.stringify(res.body)).toContain("badge");
-	});
 
-	test("Get Badge by JOB ID, JOB ID not INTEGER", async () => {
-		const res = await request.get(getBadgeByJobIdUri + "?jobId=1ssweeds");
-		//  console.log(res.body);
-		expect(res.status).toBe(500);
-		expect(JSON.stringify(res.body)).toContain("badge");
-	});
-	*/
 });
--- doer_backend/app/test/data/badge.test.data.js
+++ doer_backend/app/test/data/badge.test.data.js
@@ -4,12 +4,36 @@ const reqCreateBadge_1 = {
   icon_url: "icon.pic"
 }
 
+const reqCreateBadge_2 = {
+  name: "best badge",
+  description: "awesome badge Ever",
+  icon_url: "icon.pic"
+}
+
 const reqCreateBadge_Malformed = {
  description: "Best badge Ever",
    icon_url: "icon.pic"
 }
 
+const reqCreateBadgeAssociation_1 = {
+    "user_id": 1,
+    "badge_id": 1
+}
+
+const reqCreateBadgeAssociation_2 = {
+    "user_id": 1,
+    "badge_id": 2
+}
+
+const reqCreateBadgeAssociation_Malformed = {
+    "badge_id": 1
+}
+
 module.exports = {
   reqCreateBadge_1,
-  reqCreateBadge_Malformed
+  reqCreateBadge_2,
+  reqCreateBadgeAssociation_1,
+  reqCreateBadgeAssociation_2,
+  reqCreateBadge_Malformed,
+  reqCreateBadgeAssociation_Malformed
 }
